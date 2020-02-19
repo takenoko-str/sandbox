973126bc497037754f3bc2153f2380358c1f1407
takenoko-str <taku.hyogo.kei@gmail.com>
2020-02-19 13:16:55 +0900

    feat: run時に受け渡した環境変数の確認

--- docker/apache-wsgi/flask/flask_script.py
+++ docker/apache-wsgi/flask/flask_script.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from datetime import datetime
+import os
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
+    image_name = os.getenv('IMAGE_NAME')
+    image_tag = os.getenv('IMAGE_TAG')
     now = datetime.now()
-    return f"Hello World! { now }"
+    return f"Hello { image_name}:{ image_tag }! { now }"
--- docker/apache-wsgi/reboot.sh
+++ docker/apache-wsgi/reboot.sh
@@ -10,6 +10,9 @@ CONTAINER_ID=$(docker ps | grep "$IMAGE_NAME" | awk '{print $1}')
 lsof -i:"$PORT"
 [ $? -eq 0 ] && echo "port is already allocated" && exit
 docker build -t "$IMAGE_NAME:$IMAGE_TAG" .
-docker run -p "$PORT:$PORT" -itd "$IMAGE_NAME:$IMAGE_TAG"
+docker run -e "IMAGE_NAME=$IMAGE_NAME" \
+           -e "IMAGE_TAG=$IMAGE_TAG" \
+           -p "$PORT:$PORT" \
+           -itd "$IMAGE_NAME:$IMAGE_TAG"
 docker exec -it "$(docker ps | grep "$IMAGE_NAME" | awk '{print $1}')" /bin/bash
 
